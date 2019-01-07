06888cadb3253ea189e7d66fe341518f940c0073
neda ghafari <nnedaghafarii@gmail.com>
2019-01-07 23:15:53 +0330

    first commit

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 'use strict';
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-let arr1 = arr.slice (0,3);
-return arr1.join( "()");
-let arr2 = arr.slice(3,3);
-let arr3 = arr.slice(6,3);
-return arr3.join("-");
-let arr4 = [];
-let result = arr4.concat(arr1, arr2, arr3);
-console.log (result);
-
+function arr1 (arr) {
+  let arr1 = arr.slice(0, 3);
+  let string1 = arr1.join('');
+  arr = arr.slice(3);
+  let arr2 = arr.slice(0, 3);
+  let string2 = arr2.join('');
+  arr = arr.slice(3);
+  let arr3 = arr.slice(0, 3);
+  let string3 = arr3.join('');
+  return '(' + string1 + ')' + string2 + '-' + string3;
+}
+console.log(arr1(arr));
 // test2
 'use strit';
 let arr = [1, 2, 'a', 'b'];
 function arr1 (arr) {
-  return arr.filter(function (value) {
-    if (typeof value === 'number') {
-      return value;
-    }
-  });
+  return arr.filter(value => (typeof value === 'number'));
 }
-
 console.log(arr1(arr));
