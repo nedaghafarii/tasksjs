d63767c6d9532fd1601e882c4178a4c220104121
neda ghafari <nnedaghafarii@gmail.com>
2019-01-11 12:18:18 +0330

    first commit

--- app.js
+++ app.js
@@ -1,21 +1,2 @@
+
 'use strict';
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-function arr1 (arr) {
-  let arr1 = arr.slice(0, 3);
-  let string1 = arr1.join('');
-  arr = arr.slice(3);
-  let arr2 = arr.slice(0, 3);
-  let string2 = arr2.join('');
-  arr = arr.slice(3);
-  let arr3 = arr.slice(0, 3);
-  let string3 = arr3.join('');
-  return '(' + string1 + ')' + string2 + '-' + string3;
-}
-console.log(arr1(arr));
-// test2
-// 'use strit';
-// let arr = [1, 2, 'a', 'b'];
-// function arr1 (arr) {
-//   return arr.filter(value => (typeof value === 'number'));
-// }
-// console.log(arr1(arr));
--- test/test.js
+++ test/test.js
@@ -1,32 +1,7 @@
-// var assert = require('chai').assert;
-// describe('Array', function () {
-//   describe('#indexOf()', function () {
-//     it('should return number', function () {
-//       assert.strictEqual(arr1([1, 2, 'a', 'b']), [1, 2]);
-//     });
-//   });
-// });
 
-// function arr1 (arr) {
-//   return arr.filter(value => (typeof value === 'number'));
-// }
-// task1
 var assert = require('chai').assert;
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return digit', function () {
-      assert.strictEqual(arr1([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), '(123)456-789');
-    });
+describe('#indexOf()', function () {
+  it('should return digit', function () {
+    assert.strictEqual(arr1([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), '(123)456-789');
   });
 });
-function arr1 (arr) {
-  let arr1 = arr.slice(0, 3);
-  let string1 = arr1.join('');
-  arr = arr.slice(3);
-  let arr2 = arr.slice(0, 3);
-  let string2 = arr2.join('');
-  arr = arr.slice(3);
-  let arr3 = arr.slice(0, 3);
-  let string3 = arr3.join('');
-  return '(' + string1 + ')' + string2 + '-' + string3;
-}
