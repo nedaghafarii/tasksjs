94cb2689aafc5e84923dfe1d766f7d53bc749ec1
neda ghafari <nnedaghafarii@gmail.com>
2019-01-17 16:58:39 +0330

    tow test

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 
 'use strict';
+function sumDigits (num) {
+  let reminder = 0;
+  if (num < 0) {
+    num *= -1;
+  }
+  while (num !== 0) {
+    reminder += num % 10;
+    num = Math.floor(num / 10);
+  }
+  return reminder;
+}
+console.log(sumDigits(-32));
--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@
 var assert = require('chai').assert;
 describe('#indexOf()', function () {
   it('should return digit', function () {
-    assert.strictEqual(arr1([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), '(123)456-789');
+    assert.strictEqual(arr1(), '');
   });
 });
