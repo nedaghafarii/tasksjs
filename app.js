55013422194c3714909aa41cbe42368ea904fbd2
neda ghafari <nnedaghafarii@gmail.com>
2019-04-08 01:02:36 +0430

    first committ

--- app.js
+++ app.js
@@ -1,14 +1,165 @@
-
 'use strict';
-function sumDigits (num) {
-  let reminder = 0;
-  if (num < 0) {
-    num *= -1;
+// session 1
+
+// task1
+let user = { name: 'john', year: 30 };
+let { name, year: age, isAdmin = false } = user;
+alert(name);
+alert(age);
+alert(isAdmin);
+
+// task2
+let salaries = {
+  'john': 100,
+  'pete': 300,
+  'mary': 250
+};
+function topSalaries () {
+  let max = 0;
+  let top;
+  for (let [key, value] of Object.entries(salaries)) {
+    if (value >= max) {
+      top = key;
+      max = value;
+    }
   }
-  while (num !== 0) {
-    reminder += num % 10;
-    num = Math.floor(num / 10);
-  }
-  return reminder;
+  return top;
 }
-console.log(sumDigits(-32));
+console.log(topSalaries(salaries));
+
+// session 2
+
+// task 1
+let date = new Date(2012, 1, 20, 3, 12);
+console.log(date);
+
+// task 2
+let date = new Date(2012, 0, 3);
+function getWeekDay (date) {
+  let day = date.getDay();
+  let arr = ['sun', 'mon', 'tus', 'wed', 'thur', 'fri', 'sat' ];
+  return arr[day];
+}
+console.log(getWeekDay(date));
+
+// task 3
+let date = new Date(2012, 0, 3);
+function getWeekDay (date) {
+  let day = date.getDay();
+  let arr = ['یک شنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنج شنبه', 'جمعه', 'شنبه'];
+  return arr[day];
+}
+alert(getWeekDay(date));
+
+// task 4
+let date = new Date(2015, 0, 2);
+function getDateAgo (date, yesterday) {
+  let day = date.getDate();
+  let result = new Date(date);
+  result.setDate(day - yesterday);
+  return result.getDate();
+}
+alert(getDateAgo(date, 2));
+
+// task 5
+function getLastDayOfMonth (year, month) {
+  if (month === (0 || 2 || 4 || 6 || 7 || 9 || 11)) {
+    return 31;
+  } else if (month === (3 || 5 || 8 || 10)) {
+    return 30;
+  }
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return 29;
+  }
+  if (year % 100 === 0 && year % 400 === 0) {
+    return 29;
+  }
+  return 28;
+}
+alert(getLastDayOfMonth(2012, 1));
+
+// task 6
+function getSecondsToday () {
+  let date = new Date();
+  return (date.getHours() * 60 * 60) + (date.getMinutes() * 60) + date.getSeconds();
+}
+alert(getSecondsToday() + ' s');
+
+// task 7
+function getSecondsToTomorrow () {
+  let date = new Date();
+  return 86400 - (date.getHours() * 60 * 60) + (date.getMinutes() * 60) + (date.getSeconds());
+}
+alert(getSecondsToTomorrow() + ' s');
+
+// task 9
+let user = {
+  name: 'john smith',
+  age: 35
+};
+let json = JSON.stringify(user);
+let obj = JSON.parse(json);
+console.log(obj);
+
+// session 3
+
+// task 1
+
+function benchmark (fn) {
+  let n = 1000;
+  let start = Date.now();
+  for (let i = 0; i < 1000; i++) {
+    fn(n);
+  }
+  return Date.now() - start;
+}
+console.log('recursion:' + benchmark(sumTo) + ' ms');
+console.log('for:' + benchmark(sumToFor) + ' ms');
+
+// recursion
+function sumTo (n) {
+  return ((n - 1) === 0) ? n : n + (sumTo(n - 1));
+}
+
+// for
+function sumToFor (n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+// for fastest!
+// can not we use recursion to count sumTo(100000)
+
+// task2
+function factorial (n) {
+  return ((n === 1) ? 1 : n * factorial(n - 1));
+}
+console.log(factorial(5));
+
+// task3
+function fib (n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } return (fib(n - 1) + fib(n - 2));
+}
+console.log(fib(7));
+
+// session 4
+// task 1
+function sum (n) {
+  return function (m) {
+    return n + m;
+  };
+}
+console.log(sum(1)(2));
+
+// task 2
+let arr = [1, 2, 3, 4, 5, 6, 7];
+function inbetween (n, m) {
+  return valuse => valuse >= n && valuse <= m;
+}
+console.log(arr.filter(inbetween(3, 6)));
--- test/test.js
+++ test/test.js
@@ -1,7 +1,220 @@
-
+// session 1
+// task2
 var assert = require('chai').assert;
-describe('#indexOf()', function () {
-  it('should return digit', function () {
-    assert.strictEqual(arr1(), '');
+describe('topSalaries', function () {
+  it('should return pete', function () {
+    assert.equal(topSalaries(salaries), 'pete');
   });
 });
+let salaries = {
+  'john': 100,
+  'pete': 300,
+  'mary': 250
+};
+function topSalaries () {
+  let max = 0;
+  let top;
+  for (let [key, value] of Object.entries(salaries)) {
+    if (value >= max) {
+      top = key;
+      max = value;
+    }
+  }
+  return top;
+}
+console.log(topSalaries(salaries));
+
+// session2
+// task 2
+var assert = require('chai').assert;
+describe('get weekday', function () {
+  it('should return tus day', function () {
+    assert.equal(getWeekDay(date), 'tus');
+  });
+});
+let date = new Date(2012, 0, 3);
+function getWeekDay (date) {
+  let day = date.getDay();
+  let arr = ['sun', 'mon', 'tus', 'wed', 'thur', 'fri', 'sat' ];
+  return arr[day];
+}
+console.log(getWeekDay(date));
+
+// task 3
+var assert = require('chai').assert;
+describe('get weekday', function () {
+  it('should return سه شنبه', function () {
+    assert.equal(getWeekDay(date), 'سه شنبه');
+  });
+});
+let date = new Date(2012, 0, 3);
+function getWeekDay (date) {
+  let day = date.getDay();
+  let arr = ['یک شنبه', 'دوشنبه', 'سه شنبه', 'چهارشنبه', 'پنج شنبه', 'جمعه', 'شنبه'];
+  return arr[day];
+}
+console.log(getWeekDay(date));
+
+// task4
+var assert = require('chai').assert;
+describe('get dateago', function () {
+  it('should return dateago 1', function () {
+    assert.equal(getDateAgo(date, 1), 1);
+  });
+  it('should return dateago 2', function () {
+    assert.equal(getDateAgo(date, 2), 31);
+  });
+  it('should return dateago 365', function () {
+    assert.equal(getDateAgo(date, 365), 2);
+  });
+});
+let date = new Date(2015, 0, 2);
+function getDateAgo (date, yesterday) {
+  let day = date.getDate();
+  let result = new Date(date);
+  result.setDate(day - yesterday);
+  return result.getDate();
+}
+console.log(getDateAgo(date, 2));
+
+// task 5
+var assert = require('chai').assert;
+describe('get lastday of month', function () {
+  it('should return 29', function () {
+    assert.equal(getLastDayOfMonth(2012, 1), 29);
+  });
+});
+function getLastDayOfMonth (year, month) {
+  if (month === (0 || 2 || 4 || 6 || 7 || 9 || 11)) {
+    return 31;
+  } else if (month === (3 || 5 || 8 || 10)) {
+    return 30;
+  }
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return 29;
+  }
+  if (year % 100 === 0 && year % 400 === 0) {
+    return 29;
+  }
+  return 28;
+}
+console.log(getLastDayOfMonth(2012, 1));
+
+// session 3
+// task 1
+var assert = require('chai').assert;
+describe('sumTo', function () {
+  it('should return one', function () {
+    assert.equal(sumTo(1), 1);
+  });
+  it('should return 3', function () {
+    assert.equal(sumTo(2), 3);
+  });
+  it('should return 6', function () {
+    assert.equal(sumTo(3), 6);
+  });
+  it('should return 5050', function () {
+    assert.equal(sumTo(100), 5050);
+  });
+});
+// for
+function sumTo (n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sumTo(100));
+// recursion
+var assert = require('chai').assert;
+describe('sumTo', function () {
+  it('should return one', function () {
+    assert.equal(sumTo(1), 1);
+  });
+  it('should return 3', function () {
+    assert.equal(sumTo(2), 3);
+  });
+  it('should return 6', function () {
+    assert.equal(sumTo(3), 6);
+  });
+  it('should return 5050', function () {
+    assert.equal(sumTo(100), 5050);
+  });
+});
+function sumTo (n) {
+  return ((n - 1) === 0) ? n : n + (sumTo(n - 1));
+}
+console.log(sumTo(1));
+
+// task 2
+var assert = require('chai').assert;
+describe('factorial', function () {
+  it('should return 1', function () {
+    assert.equal(factorial(1), 1);
+  });
+  it('should return 2', function () {
+    assert.equal(factorial(2), 2);
+  });
+  it('should return 6', function () {
+    assert.equal(factorial(3), 6);
+  });
+  it('should return 120', function () {
+    assert.equal(factorial(5), 120);
+  });
+});
+function factorial (n) {
+  return ((n === 1) ? 1 : n * factorial(n - 1));
+}
+console.log(factorial(5));
+
+// task3
+var assert = require('chai').assert;
+describe('fibonacci', function () {
+  it('should return 2', function () {
+    assert.equal(fib(3), 2);
+  });
+  it('should return 7', function () {
+    assert.equal(fib(7), 13);
+  });
+});
+function fib (n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } return (fib(n - 1) + fib(n - 2));
+}
+console.log(fib(3));
+
+// session 4
+// task1
+var assert = require('chai').assert;
+describe('sum numbers', function () {
+  it('should return 3', function () {
+    assert.equal(sum(1)(2), 3);
+  });
+  it('should return 4', function () {
+    assert.equal(sum(5)(-1), 4);
+  });
+});
+function sum (n) {
+  return function (m) {
+    return n + m;
+  };
+}
+console.log(sum(1)(2));
+
+// task2
+var assert = require('chai').assert;
+describe('inbetween', function () {
+  it('should return [3, 4, 5, 6]', function () {
+    assert.deepEqual(arr.filter(inbetween(3, 6)), [3, 4, 5, 6]);
+  });
+});
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+function inbetween (n, m) {
+  return valuse => valuse >= n && valuse <= m;
+}
+console.log(arr.filter(inbetween(3, 6)));
